218f2842fb511e37e4d7a7f8db04a71d69fc3767
Arjan <info@skoften.net>
2018-11-22 13:14:06 +0100

    Add option to disable clear caches

--- src/config.php
+++ src/config.php
@@ -60,6 +60,9 @@ return [
 	// if a upload location has a subfolder defined, add this to the transcoder paths too
 	'createSubfolders' => true,
 	
+	// clear caches when somebody clears all caches from the CP?
+	'clearCaches' => false,
+	
     // Preset video encoders
     'videoEncoders' => [
         'h264' => [
