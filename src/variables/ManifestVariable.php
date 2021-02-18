cbede7896d40cf902b70d05f38b87693db478c12
Andrew Welch <andrew@keluli.local>
2021-02-18 16:23:14 -0500

    Intial manifest work

--- src/variables/ManifestVariable.php
+++ src/variables/ManifestVariable.php
@@ -8,127 +8,69 @@ use nystudio107\transcoder\assetbundles\transcoder\TranscoderAsset;
 use Craft;
 use craft\helpers\Template;
 
+use yii\base\InvalidConfigException;
 use yii\web\NotFoundHttpException;
 
 use Twig\Markup;
 
 class ManifestVariable
 {
-    // Protected Static Properties
-    // =========================================================================
-
-    protected static $config = [
-        // If `devMode` is on, use webpack-dev-server to all for HMR (hot module reloading)
-        'useDevServer' => true,
-        // Manifest names
-        'manifest'     => [
-            'legacy' => 'manifest.json',
-            'modern' => 'manifest.json',
-        ],
-        // Public server config
-        'server'       => [
-            'manifestPath' => '/',
-            'publicPath' => '/',
-        ],
-        // webpack-dev-server config
-        'devServer'    => [
-            'manifestPath' => 'http://127.0.0.1:8080',
-            'publicPath' => '/',
-        ],
-    ];
-
     // Public Methods
     // =========================================================================
 
     /**
-     * ManifestVariable constructor.
+     * Get the passed in JS modules from the manifest, and register them in the current Craft view
+     *
+     * @param array $modules
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
      */
-    public function __construct()
+    public function registerJsModules(array $modules)
     {
-        ManifestHelper::invalidateCaches();
-        $bundle = new TranscoderAsset();
-        $baseAssetsUrl = Craft::$app->assetManager->getPublishedUrl(
-            $bundle->sourcePath,
-            true
-        );
-        self::$config['server']['manifestPath'] = Craft::getAlias($bundle->sourcePath);
-        self::$config['server']['publicPath'] = $baseAssetsUrl;
-        $useDevServer = getenv('NYS_PLUGIN_DEVSERVER');
-        if ($useDevServer !== false) {
-            self::$config['useDevServer'] = (bool)$useDevServer;
-        }
+        ManifestHelper::registerJsModules($modules);
     }
 
     /**
+     * Get the passed in CS modules from the manifest, and register them in the current Craft view
+     *
+     * @param array $modules
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
+     */
+    public function registerCssModules(array $modules)
+    {
+        ManifestHelper::registerCssModules($modules);
+    }
+
+    /**
+     * Get the passed in JS module from the manifest, then output a `<script src="">` tag for it in the HTML
+     *
      * @param string     $moduleName
      * @param bool       $async
-     * @param null|array $config
      *
-     * @return null|\Twig_Markup
-     * @throws \yii\web\NotFoundHttpException
+     * @return null|Markup
+     * @throws NotFoundHttpException
      */
-    public function includeCssModule(string $moduleName, bool $async = false, $config = null)
+    public function includeJsModule(string $moduleName, bool $async = false)
     {
         return Template::raw(
-            ManifestHelper::getCssModuleTags(self::$config, $moduleName, $async) ?? ''
-        );
-    }
-
-
-    /**
-     * Returns the uglified loadCSS rel=preload Polyfill as per:
-     * https://github.com/filamentgroup/loadCSS#how-to-use-loadcss-recommended-example
-     *
-     * @return string
-     */
-    public static function includeCssRelPreloadPolyfill(): string
-    {
-        return Template::raw(
-            ManifestHelper::getCssRelPreloadPolyfill() ?? ''
+            ManifestHelper::includeJsModule($moduleName, $async) ?? ''
         );
     }
 
     /**
+     * Get the passed in CS module from the manifest, then output a `<link>` tag for it in the HTML
+     *
      * @param string     $moduleName
      * @param bool       $async
-     * @param null|array $config
      *
-     * @return null|\Twig_Markup
-     * @throws \yii\web\NotFoundHttpException
+     * @return Markup
+     * @throws NotFoundHttpException
      */
-    public function includeJsModule(string $moduleName, bool $async = false, $config = null)
+    public function includeCssModule(string $moduleName, bool $async = false): Markup
     {
         return Template::raw(
-            ManifestHelper::getJsModuleTags(self::$config, $moduleName, $async) ?? ''
-        );
-    }
-
-    /**
-     * Return the URI to a module
-     *
-     * @param string $moduleName
-     * @param string $type
-     * @param null   $config
-     *
-     * @return null|\Twig_Markup
-     * @throws \yii\web\NotFoundHttpException
-     */
-    public function getModuleUri(string $moduleName, string $type = 'modern', $config = null)
-    {
-        return Template::raw(
-            ManifestHelper::getModule(self::$config, $moduleName, $type) ?? ''
-        );
-    }
-
-    /**
-     * Include the Safari 10.1 nomodule fix JavaScript
-     *
-     * @return \Twig_Markup
-     */
-    public function includeSafariNomoduleFix()
-    {
-        return Template::raw(
-            ManifestHelper::getSafariNomoduleFix() ?? ''
+            ManifestHelper::includeCssModule($moduleName, $async) ?? ''
         );
     }
 }
