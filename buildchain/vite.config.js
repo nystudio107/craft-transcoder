31e0dbda7eeb41b4c1f9601821f3f5c020665d93
Andrew Welch <andrew@nystudio107.com>
2021-05-09 01:44:47 -0400

    Code format
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- buildchain/vite.config.js
+++ buildchain/vite.config.js
@@ -1,6 +1,6 @@
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
 import ViteRestart from 'vite-plugin-restart';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ command }) => ({
