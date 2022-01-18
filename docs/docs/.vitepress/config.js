d6e1c9c149eb5a1acdc949f8fabf90a528850355
Andrew Welch <andrew@keluli.local>
2022-01-18 15:46:47 -0500

    refactor: Use new DocSearch appId & appKey

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -1,29 +1,30 @@
 module.exports = {
-    title: 'Transcoder Plugin Documentation',
-    description: 'Documentation for the Transcoder plugin',
-    base: '/docs/transcoder/',
-    lang: 'en-US',
-    head: [
-        ['meta', { content: 'https://github.com/nystudio107', property: 'og:see_also', }],
-        ['meta', { content: 'https://www.youtube.com/channel/UCOZTZHQdC-unTERO7LRS6FA', property: 'og:see_also', }],
-        ['meta', { content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also', }],
-    ],
-    themeConfig: {
-        repo: 'nystudio107/craft-transcoder',
-        docsDir: 'docs/docs',
-        docsBranch: 'develop',
-        algolia: {
-            apiKey: '14f28d1e87ded365acc21d0e921732e6',
-            indexName: 'transcoder'
-        },
-        editLinks: true,
-        editLinkText: 'Edit this page on GitHub',
-        lastUpdated: 'Last Updated',
-        sidebar: [
-            { text: 'Transcoder Plugin', link: '/' },
-            { text: 'Transcoder Overview', link: '/overview.html' },
-            { text: 'Configuring Transcoder', link: '/configuring.html' },
-            { text: 'Using Transcoder', link: '/using.html' },
-        ],
+  title: 'Transcoder Plugin Documentation',
+  description: 'Documentation for the Transcoder plugin',
+  base: '/docs/transcoder/',
+  lang: 'en-US',
+  head: [
+    ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.youtube.com/channel/UCOZTZHQdC-unTERO7LRS6FA', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also',}],
+  ],
+  themeConfig: {
+    repo: 'nystudio107/craft-transcoder',
+    docsDir: 'docs/docs',
+    docsBranch: 'develop',
+    algolia: {
+      appId: 'VWUWF9S521',
+      apiKey: 'db5c03f88e474cbf0356841089be7ffa',
+      indexName: 'transcoder'
     },
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+    sidebar: [
+      {text: 'Transcoder Plugin', link: '/'},
+      {text: 'Transcoder Overview', link: '/overview.html'},
+      {text: 'Configuring Transcoder', link: '/configuring.html'},
+      {text: 'Using Transcoder', link: '/using.html'},
+    ],
+  },
 };
