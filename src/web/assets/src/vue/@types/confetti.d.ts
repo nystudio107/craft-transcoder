bdb705400d5e94bd715a4fc0c6f15df810753fcf
Andrew Welch <andrew@nystudio107.com>
2022-07-16 14:09:58 -0700

    refactor: Use enums in .d.ts properly

--- src/web/assets/src/vue/@types/confetti.d.ts
+++ src/web/assets/src/vue/@types/confetti.d.ts
@@ -1,32 +1,34 @@
 declare module "vue-confetti/src/confetti.js";
 
 interface ConfettiInterface {
-    start(opts: Partial<ConfettiConfig>): void,
-    update(opts: Partial<ConfettiConfig>): void,
-    stop(): void,
+  start(opts: Partial<ConfettiConfig>): void,
+
+  update(opts: Partial<ConfettiConfig>): void,
+
+  stop(): void,
 }
 
-enum ParticlesType {
-    circle = "circle",
-    rect = "rect",
-    heart = "heart",
-    image = "image",
+export enum ParticlesType {
+  circle = "circle",
+  rect = "rect",
+  heart = "heart",
+  image = "image",
 }
 
 interface ParticlesConfig {
-    type : ParticlesType,
-    size : number,
-    dropRate : number,
-    colors : string[],
-    url : string | null,
+  type: ParticlesType,
+  size: number,
+  dropRate: number,
+  colors: string[],
+  url: string | null,
 }
 
 interface ConfettiConfig {
-    particles : Partial<ParticlesConfig>[],
-    defaultType : ParticlesType,
-    defaultSize : number,
-    defaultDropRate : number,
-    defaultColors : string[],
-    canvasId : number,
-    particlesPerFrame : number,
+  particles: Partial<ParticlesConfig>[],
+  defaultType: ParticlesType,
+  defaultSize: number,
+  defaultDropRate: number,
+  defaultColors: string[],
+  canvasId: number,
+  particlesPerFrame: number,
 }
