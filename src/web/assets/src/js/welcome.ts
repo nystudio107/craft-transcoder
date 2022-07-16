143c3377bd59d2238b77025726c076eaf48d9c8f
Andrew Welch <andrew@nystudio107.com>
2022-07-16 14:10:15 -0700

    refactor: Add accept HMR

--- src/web/assets/src/js/welcome.ts
+++ src/web/assets/src/js/welcome.ts
@@ -1,17 +1,24 @@
 import App from '@/vue/App.vue';
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 
 // App main
 const main = async () => {
-    // Create our vue instance
-    const app = createApp(App);
-    // Mount the app
-    const root = app.mount('#app-container');
+  // Create our vue instance
+  const app = createApp(App);
+  // Mount the app
+  const root = app.mount('#app-container');
 
-    return root;
+  return root;
 };
 
 // Execute async function
-main().then( () => {
-    console.log();
+main().then(() => {
+  console.log();
 });
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR")
+  });
+}
