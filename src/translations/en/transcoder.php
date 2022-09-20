83accd25d231ee68b87e592ee0ca8e837239ec2f
Andrew Welch <andrew@keluli.local>
2022-09-20 13:08:43 -0400

    chore: Craft CMS 3.x -> Craft CMS

--- src/translations/en/transcoder.php
+++ src/translations/en/transcoder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Transcoder plugin for Craft CMS 3.x
+ * Transcoder plugin for Craft CMS
  *
  * Transcode videos to various formats, and provide thumbnails of the video
  *
@@ -14,8 +14,8 @@
  * @since     1.0.0
  */
 return [
-    'Transcoder caches'              => 'Transcoder caches',
-    '{name} plugin loaded'           => '{name} plugin loaded',
+    'Transcoder caches' => 'Transcoder caches',
+    '{name} plugin loaded' => '{name} plugin loaded',
     '{name} cache directory cleared' => '{name} cache directory cleared',
     'Manifest file not found at: {manifestPath}' => 'Manifest file not found at: {manifestPath}',
     'Module does not exist in the manifest: {moduleName}' => 'Module does not exist in the manifest: {moduleName}'
