199a4f0d3b01330241b34c3bee981621b244216c
Andrew Welch <andrew@nystudio107.com>
2026-08-17 23:37:25 -0400

    refactor: Update build system

---
+++ buildchain/tailwind.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+// No way to do this via CSS config yet, ref: https://github.com/tailwindlabs/tailwindcss/issues/15720
+  blocklist: [
+    'static',
+  ],
+};
